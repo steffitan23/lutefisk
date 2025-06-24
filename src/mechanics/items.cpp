47397dac53b93c3abf6a4293e8397bd1a0cd44f5
steffitan23 <steffitlw@berkeley.edu>
2025-06-24 17:15:20 +0800

    Create scannable and unscannable items

--- src/mechanics/items.cpp
+++ src/mechanics/items.cpp
@@ -1,53 +1,48 @@
 #include "items.h"
 
-std::unique_ptr<Item> Item::create_item(const std::string &id, const sf::Texture &tex)
+Item::Item(std::string name,
+           sf::Vector2f position,
+           sf::Vector2f size,
+           const sf::Texture &texture) : m_name{name},
+                                         m_texture{texture},
+                                         m_sprite{texture}
+
+{
+    m_sprite.setPosition(position);
+    const auto bounds = m_sprite.getLocalBounds();
+    if (bounds.size.x > 0 && bounds.size.y > 0)
+        m_sprite.setScale(sf::Vector2f{size.x / bounds.size.x, size.y / bounds.size.y});
+}
+
+ScannableItem::ScannableItem(std::string name,
+                             sf::Vector2f position,
+                             sf::Vector2f size,
+                             const sf::Texture &texture,
+                             float price)
+    : Item::Item(std::move(name), position, size, texture), m_price{price}, m_scanned{false}, m_bagged{false} {}
+
+std::unique_ptr<ScannableItem> Item::create_scannable_item(const std::string &id, const sf::Texture &tex, float price)
 {
     if (id == "tomato_soup")
     {
-        return std::make_unique<Item>(ItemConfig{"Tomato Soup", 2.5f, {160.f, 600.f}, {128.f, 256.f}, tex});
+        return std::make_unique<ScannableItem>(ScannableItem{std::string{"Tomato Soup"}, {160.f, 600.f}, {128.f, 256.f}, tex, 2.5f});
     }
     else if (id == "appletron")
     {
-        return std::make_unique<Item>(ItemConfig{"Appletron", 17.0f, {180.f, 600.f}, {256.f, 256.f}, tex});
+        return std::make_unique<ScannableItem>(ScannableItem{std::string{"Appletron"}, {180.f, 600.f}, {256.f, 256.f}, tex, 17.0f});
     }
 
     throw std::invalid_argument("Unknown item type: " + id);
 }
 
-Item::Item(const ItemConfig &config)
-    : m_name{config.name},
-      m_price{config.price},
-      m_scanned{false},
-      m_bagged{false},
-      m_texture{config.texture},
-      m_sprite{config.texture}
-
-{
-    m_sprite.setPosition(config.position);
-    const auto bounds = m_sprite.getLocalBounds();
-    if (bounds.size.x > 0 && bounds.size.y > 0)
-        m_sprite.setScale(sf::Vector2f{config.size.x / bounds.size.x, config.size.y / bounds.size.y});
-}
+// TODO: make unscannable items
 
 std::string Item::get_name() const { return m_name; }
-float Item::get_price() const { return m_price; }
 
 void Item::set_pos(const sf::Vector2f &vec) { m_sprite.setPosition(vec); }
 void Item::set_pos(float x, float y) { set_pos(sf::Vector2f{x, y}); }
 
 sf::Vector2f Item::get_pos() const { return m_sprite.getPosition(); }
-
-bool Item::get_scanned() const { return m_scanned; }
-
-void Item::scan()
-{
-    m_scanned = true;
-} // TODO: change.... maybe make private?
-
-void Item::bag()
-{
-    m_bagged = true;
-} // TODO: change.... maybe make private?
 
 sf::FloatRect Item::get_bounds() const { return m_sprite.getGlobalBounds(); }
 
@@ -61,3 +56,17 @@ void Item::draw(sf::RenderTarget &target, sf::RenderStates states) const
     states.transform *= getTransform();
     target.draw(m_sprite, states);
 }
+
+float ScannableItem::get_price() const { return m_price; }
+
+bool ScannableItem::get_scanned() const { return m_scanned; }
+
+void ScannableItem::scan()
+{
+    m_scanned = true;
+} // TODO: change.... maybe make private?
+
+void ScannableItem::bag()
+{
+    m_bagged = true;
+} // TODO: change.... maybe make private?
--- src/mechanics/items.h
+++ src/mechanics/items.h
@@ -2,41 +2,62 @@
 
 #include <SFML/Graphics.hpp>
 
-struct ItemConfig
-{
-    std::string name;
-    float price;
-    sf::Vector2f position;
-    sf::Vector2f size;
-    const sf::Texture &texture;
-};
+class ScannableItem;
+class UnscannableItem;
 
 class Item : public sf::Drawable, public sf::Transformable
 {
 public:
-    Item(const ItemConfig &config);
+    Item(std::string name,
+         sf::Vector2f position,
+         sf::Vector2f size,
+         const sf::Texture &texture);
 
     virtual ~Item() = default;
     std::string get_name() const;
-    float get_price() const;
+
     void set_pos(const sf::Vector2f &vec);
     void set_pos(float x, float y);
     sf::Vector2f get_pos() const;
-    bool get_scanned() const;
-    virtual void scan();
-    void bag();
+
     sf::FloatRect get_bounds() const;
     sf::Vector2f get_center() const;
-    static std::unique_ptr<Item> create_item(const std::string &id, const sf::Texture &texture);
+
+    static std::unique_ptr<ScannableItem> create_scannable_item(const std::string &id, const sf::Texture &texture, float price);
+    static std::unique_ptr<UnscannableItem> create_unscannable_item(const std::string &id, const sf::Texture &texture);
+
+    virtual float get_price() const { return 0.0f; };
+    virtual bool get_scanned() const { return false; };
+    virtual void scan() {};
+    virtual void bag() {};
 
 protected:
     void draw(sf::RenderTarget &target, sf::RenderStates states) const override;
 
 private:
-    float m_price;
-    bool m_scanned;
-    bool m_bagged;
     const sf::Texture &m_texture;
     sf::Sprite m_sprite;
     std::string m_name;
+};
+
+class ScannableItem : public Item
+{
+public:
+    ScannableItem(std::string name, sf::Vector2f position, sf::Vector2f size, const sf::Texture &texture, float price);
+    float get_price() const override;
+    bool get_scanned() const override;
+    void scan() override;
+    void bag() override;
+
+private:
+    float m_price;
+    bool m_scanned;
+    bool m_bagged;
+};
+
+class UnscannableItem : public Item
+{
+public:
+    UnscannableItem(std::string name, sf::Vector2f position, sf::Vector2f size, const sf::Texture &texture)
+        : Item(std::move(name), position, size, texture) {}
 };
